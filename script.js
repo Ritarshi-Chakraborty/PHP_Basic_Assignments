7174d6b334daea0624338652ffadc99e53c4b5ee
Ritarshi-Chakraborty <ritarshi.chakraborty@innoraft.com>
2025-03-28 18:23:34 +0530

    Fixed the fullname input handler

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-$(document).ready(function handleName() {
-
-
+$(document).ready(function () {
+    // handling the Fullname input
     function handleName() {
-        // Handling the Fullname input field
-        $('input[name="first_name"]').on('input', function() {
-            let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
-            $('input[name="full_name"]').val(fullName);
-        })
-        $('input[name="last_name"]').on('input', function() {
+        $('input[name="first_name"], input[name="last_name"]').on('input', function() {
             let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
             $('input[name="full_name"]').val(fullName);
         })
     }
 
+    handleName(); 
 })
