5df4848c71a41ddb0208d9e0aece6a01f32db4e1
Ritarshi-Chakraborty <ritarshi.chakraborty@innoraft.com>
2025-03-28 18:08:12 +0530

    Completed assignment 2 and implemented proper OOP concepts

--- response.php
+++ response.php
@@ -1,3 +1,40 @@
+<!-- Handling the uploaded image -->
+<?php
+    class ImagePath {
+        protected $targetFile;
+
+        function __construct($targetDir) {
+            $this->targetFile = $targetDir . basename($_FILES["image"]["name"]);
+            move_uploaded_file($_FILES["image"]["tmp_name"], $this->targetFile);
+        }
+
+        function returnPath() {
+            return $this->targetFile;
+        }
+    }
+
+    $uploaded_image = new ImagePath("./images/");
+    $imageSrc = $uploaded_image->returnPath();
+
+    class displayResult {
+        protected $table_array;
+
+        function returnResult() {
+            $this->table_array = explode("\n", $_POST['result']);
+            foreach ($this->table_array as $key => $value) {
+                $subject = explode('|', $value)[0];
+                $marks = explode('|', $value)[1];
+                echo "<tr>
+                    <td>$subject</td>
+                    <td>$marks</td>
+                </tr>";
+            }
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,29 +47,27 @@
     <div class="container">
         <!-- Welcoming the user with his/her name -->
         <h1><?php echo "Hello, ".$_POST['first_name']." ".$_POST['last_name']."!"; ?></h1>
-
+        
         <!-- Displaying the uploaded image -->
-        <?php
-            // $targetDir = "/images/";
-            // $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-            // if (! is_writable("/images/")) {
-            //     die("Error: 'images/' folder is not writable.");
-            // }
-            // move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile);
-        ?>
-        <img src="<?php echo $targetFile; ?>" alt="Uploaded Image">
-
+        <div class="image-wrapper">
+            <img src="<?php echo $imageSrc; ?>" alt="Uploaded Image">
+        </div>
+       
         <!-- Displaying the subject & marks from textarea in a table format -->
-        <?php
-            $table_array =  explode("\n", $_POST['result']);
-            foreach ($table_array as $key => $value) {
-                echo "$key => $value";
-            } 
-        ?>
+        
         <table>
-            <tr>
-                
-            </tr>
+            <thead>
+                <tr>
+                    <th>Subject</th>
+                    <th>Marks</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $result = new displayResult();
+                    $result->returnResult();
+                ?>
+            </tbody>
         </table>
     </div>
 </body>
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    
-    // Handling the Fullname input field
-    $('input[name="first_name"]').on('input', function() {
-        let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
-        $('input[name="full_name"]').val(fullName);
-    })
-    $('input[name="last_name"]').on('input', function() {
-        let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
-        $('input[name="full_name"]').val(fullName);
-    })
+$(document).ready(function handleName() {
 
+
+    function handleName() {
+        // Handling the Fullname input field
+        $('input[name="first_name"]').on('input', function() {
+            let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
+            $('input[name="full_name"]').val(fullName);
+        })
+        $('input[name="last_name"]').on('input', function() {
+            let fullName = $('input[name="first_name"]').val() + " " + $('input[name="last_name"]').val();
+            $('input[name="full_name"]').val(fullName);
+        })
+    }
 
 })
